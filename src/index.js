ec279d4e76983bdfd8f1e6d393620e31cc0de5a5
Michael <michaelleojacob@gmail.com>
2022-01-08 14:26:26 -0800

    following the react docs, have a working clock.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ==================
+//? < basic >
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const element = <h1>Hello, world!</h1>;
+// ReactDOM.render(element, document.getElementById('root'));
+
+//? </ basic >
+// ==================
+
+// ==================
+//? < updating the rendered element >
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+//* every second, the setInterval will run tick which invokes ReactDOM.render()
+setInterval(tick, 1000);
+
+//? </ updating the rendered element >
+// ==================
